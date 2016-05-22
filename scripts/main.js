5dc27085c7d085eb08a19b45634bc325e2a064d7
sebaek <sebaek@gmail.com>
2016-05-22 21:40:16 +0900

    added bootstrap css

--- scripts/main.js
+++ scripts/main.js
@@ -1,8 +1,15 @@
 var Main = React.createClass({
 	render: function() {
 		return (
-			<div className="">
+			<div className="jumbotron">
+			<h1>
 			Hello, World!
+			</h1>
+			<p>
+			Something like description......
+			</p>
+			<p><a className="btn btn-primary btn-lg" href="#" role="button">Get List</a></p>
+
 			</div>
 		);
 	}
