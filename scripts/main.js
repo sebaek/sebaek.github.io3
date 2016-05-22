54775e51bf88b313b38daf1da153e9ab86e95532
sebaek <sebaek@gmail.com>
2016-05-22 21:22:45 +0900

    init commit

---
+++ scripts/main.js
@@ -0,0 +1,15 @@
+var Main = React.createClass({
+	render: function() {
+		return (
+			<div className="">
+			Hello, World!
+			</div>
+		);
+	}
+});
+
+
+ReactDOM.render(
+	<Main />,
+	document.getElementId('content')
+);
