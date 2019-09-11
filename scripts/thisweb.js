fbf7060091c0ce959f65d388c81a3f7108a8689f
sebaek <sebaek@gmail.com>
2019-09-11 15:41:57 +0900

    material css navbar

---
+++ scripts/thisweb.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', function () {
+    var options = {};
+    var elems = document.querySelectorAll('.sidenav');
+    var instances = M.Sidenav.init(elems, options);
+});
