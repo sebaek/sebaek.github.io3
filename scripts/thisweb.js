5839538c808ddfa594ecdafc295aa506eae6b44e
sebaek <sebaek@gmail.com>
2019-09-11 16:29:14 +0900

    carousel

--- scripts/thisweb.js
+++ scripts/thisweb.js
@@ -1,5 +1,10 @@
 document.addEventListener('DOMContentLoaded', function () {
-    var options = {};
-    var elems = document.querySelectorAll('.sidenav');
-    var instances = M.Sidenav.init(elems, options);
+    // let options = {};
+    // let elems = document.querySelectorAll('.sidenav');
+    // let instances = M.Sidenav.init(elems, options);
+    M.AutoInit();
+    var toastHTML = '<span>I am toast content</span><button class="btn-flat toast-action">Undo</button>';
+    M.toast({html: toastHTML});
+    // let instance = M.toast({html: toastHTML});
+    // console.log(instance.timeRemaining);
 });
